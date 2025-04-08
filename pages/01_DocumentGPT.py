50504825b9a92dddcc336aa5be67494a81b74490
feelsuegood <suaj2021@gmail.com>
2025-04-09 07:24:59 +1000

    #7.7 chat history

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -14,13 +14,11 @@ st.set_page_config(
     page_icon="📄",
 )
 
-st.title("DocumentGPT")
 
-
+@st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
     file_path = f"./.cache/files/{file.name}"
-    st.write(file_content, file_path)
     with open(file_path, "wb") as f:
         f.write(file_content)
         cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
@@ -29,7 +27,7 @@ def embed_file(file):
             chunk_size=600,
             chunk_overlap=100,
         )
-        loader = UnstructuredFileLoader("./files/preface.docx")
+        loader = UnstructuredFileLoader(file_path)
         docs = loader.load_and_split(text_splitter=splitter)
         embeddings = OpenAIEmbeddings()
         cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
@@ -44,19 +42,42 @@ def embed_file(file):
         return retriever
 
 
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
+
+
+st.title("DocumentGPT")
+
 st.markdown(
     """
 Welcome!
 
 Use this chatbot to ask a question to an AI about your files!
+
+Upload your files on the sidebar.
 """
 )
 
-file = st.file_uploader(
-    "Upload a .txt, .pdf, or .docx file", type=["txt", "pdf", "docx"]
-)
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a .txt, .pdf, or .docx file", type=["txt", "pdf", "docx"]
+    )
 
 if file:
     retriever = embed_file(file)
-    s = retriever.invoke("Characteristics of Jane Austen")
-    s
+    send_message("I'm ready. Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your files...")
+    if message:
+        send_message(message, "human")
+        send_message("test", "ai")
+else:
+    st.session_state["messages"] = []
