3ec5a288936f2abd6ff73097775de4dda881096e
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-05-18 22:26:12 +1000

    Enhance main.py with new authorization endpoints and consequential flag for OpenAI integration; remove note.md file.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from http import server
-from fastapi import FastAPI
+from typing import Any
+from fastapi import Body, FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel, Field
 
 app = FastAPI(
@@ -26,9 +27,53 @@ class Treat(BaseModel):
     description="Upon receiving a GET request this endpoint will return a love magic treat given by Cutie Sueweetie herself.",
     response_description="A treat object that contains the treat given by Cutie Sueweetie and the level that the love magic treat has from 0 to 100.",
     response_model=Treat,
+    openapi_extra={
+        "x-openai-isConsequential": True,
+        # If the x-openai-isConsequential field is true, ChatGPT treats the operation as "must always prompt the user for confirmation before running" and don't show an "always allow" button (both are features of GPTs designed to give builders and users more control over actions).
+        # If the x-openai-isConsequential field is false, ChatGPT shows the "always allow button".
+        # If the field isn't present, ChatGPT defaults all GET operations to false and all other operations to true
+    },
 )
-def get_treat():
+def get_treat(request: Request):
+    print(request.headers)
     return {
         "treat": "Action speaks louder than words.",
         "level": 99,
+    }
+
+
+user_token_db = {"ABCDEF": "sueweetie"}
+
+
+@app.get(
+    "/authorize",
+    response_class=HTMLResponse,
+    include_in_schema=False,
+)
+def handle_authorize(
+    client_id: str,
+    redirect_uri: str,
+    state: str,
+):
+    return f"""
+    <html>
+        <head>
+            <title>Cutie Sueweetie Log In</title>
+        </head>
+        <body>
+            <h1>Log Into Cutie Sueweetie</h1>
+            <a href="{redirect_uri}?code=ABCDEF&state={state}">Authorize Cutie Sueweetie GPT</a>
+        </body>
+    </html>
+    """
+
+
+@app.post(
+    "/token",
+    include_in_schema=False,
+)
+def handle_token(code=Form(...)):
+    print(code)
+    return {
+        "access_token": user_token_db[code],
     }
