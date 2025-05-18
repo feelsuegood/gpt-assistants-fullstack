5b7e6a91d50e391af268b2e50672114daccb6451
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-05-19 07:23:00 +1000

    chefGPT - custom GPT, oauth, api key -, pinecone vector store

--- main.py
+++ main.py
@@ -1,45 +1,65 @@
-from typing import Any
-from fastapi import Body, FastAPI, Form, Request
+from fastapi import FastAPI, Form
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
+from pinecone import Pinecone
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+pc = Pinecone(
+    api_key=os.environ.get("PINECONE_API_KEY"),
+)
+
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
+index_name = "recipes"
+
+index = pc.Index(index_name)
+
+vector_store = PineconeVectorStore(
+    index=index,
+    embedding=embeddings,
+)
 
 app = FastAPI(
-    title="Cutie Sueweetie Treats Giver",
-    description="Get a love magic treats given by Cutie Sueweetie herself.",
+    title="ChefGPT. The best provider of Indian Recipes in the world",
+    description="Give ChatGPT a couple of ingredients and it will give you recipes in retrun.",
     servers=[
         {
-            "url": "https://immigrants-pray-albania-lips.trycloudflare.com",
+            "url": "https://editor-possibilities-baskets-sponsorship.trycloudflare.com",
         },
     ],
 )
 
 
-class Treat(BaseModel):
-    treat: str = Field(description="The love magic treat that Cutie Sweetie gave.")
-    level: int = Field(
-        description="The level of treat that represents how powerful it is."
-    )
+class Document(BaseModel):
+    page_content: str
 
 
 @app.get(
-    "/treat",
-    summary="Returns a random treat by Cutie Sueweetie",
-    description="Upon receiving a GET request this endpoint will return a love magic treat given by Cutie Sueweetie herself.",
-    response_description="A treat object that contains the treat given by Cutie Sueweetie and the level that the love magic treat has from 0 to 100.",
-    response_model=Treat,
+    "/recipes",
+    summary="Returns a list of recipes.",
+    description="Upon receiving an ingredient, this endpoint will return a list of recipes that contain that ingredient.",
+    response_description="A document object that contains the recipe and preparation instructions",
+    response_model=list[Document],
     openapi_extra={
-        "x-openai-isConsequential": True,
+        "x-openai-isConsequential": False,
         # If the x-openai-isConsequential field is true, ChatGPT treats the operation as "must always prompt the user for confirmation before running" and don't show an "always allow" button (both are features of GPTs designed to give builders and users more control over actions).
         # If the x-openai-isConsequential field is false, ChatGPT shows the "always allow button".
         # If the field isn't present, ChatGPT defaults all GET operations to false and all other operations to true
     },
 )
-def get_treat(request: Request):
-    print(request.headers)
-    return {
-        "treat": "Action speaks louder than words.",
-        "level": 99,
-    }
+# /recipes?ingredient=tofu
+def get_recipe(ingredient: str):
+    docs = vector_store.similarity_search(
+        ingredient,
+        k=2,
+    )
+    return docs
 
 
 user_token_db = {"ABCDEF": "sueweetie"}
