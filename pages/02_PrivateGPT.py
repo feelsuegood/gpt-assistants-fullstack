fb57ee31d63b561f89bc10618dd162023ae25ec6
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-05-27 16:22:45 +1000

    Enhance availability check for Ollama server in PrivateGPT and update user notification for cloud environment restrictions.

--- pages/02_PrivateGPT.py
+++ pages/02_PrivateGPT.py
@@ -1,4 +1,3 @@
-import os
 from langchain_ollama import ChatOllama
 from langchain_ollama import OllamaEmbeddings
 from langchain.prompts import ChatPromptTemplate
@@ -160,9 +159,25 @@ Question: {question}
 Answer: """
 )
 
+
+def is_ollama_available():
+    try:
+        # check if ollama is available
+        response = requests.get("http://localhost:11434/api/tags", timeout=1)
+        return response.status_code == 200
+    except requests.exceptions.RequestException:
+        return False
+
+
 # in streamlit cloud, send a message to a user that this app is not available
-if os.getenv("STREAMLIT_CLOUD", "") == "true":
-    st.markdown("Sorry, this app is not available in the cloud environment 😿")
+if not is_ollama_available():
+    st.markdown(
+        """
+        # Sorry 😿
+        - This app is not available:
+            - Cloud Environment 
+            - When Ollama Server is not running """
+    )
 else:
     st.markdown(
         """
